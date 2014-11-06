9fa8a6a9dd3e91c43837ef049ba004a5ea1de00f
Archis Gore <archisgore@yahoo.com>
2014-11-05 20:59:05 -0800

    Index

---
+++ src/index.erl
@@ -0,0 +1,5 @@
+-module(index).
+-compile(export_all).
+-include__lib("n2o/include/wf.hrl").
+
+main() -> "Hello".
---
+++ start.bat
@@ -0,0 +1,1 @@
+erl -pa ebin deps/cowboy/ebin deps/erlydtl/ebin deps/gproc/ebin deps/n2o/ebin deps/ranch/ebin -s divepredictor
