89798e08c3526be4e17ea9aeeb0c11495ba1525f
Archis Gore <archisgore@yahoo.com>
2014-11-16 18:09:03 -0800

    Set port to 80

--- src/index.erl
+++ src/index.erl
@@ -17,12 +17,12 @@ content() -> [#form{body=[#table{class=["standardTable"],
 				]}],
 
 			body=[#tr{cells=[
-					#td{body=[#select{body=selectable_divesites(), multiple=false, required=true, id=diveSite}]},
-					#td{body=[#date{id=startDate, required=true, value=selectedStartDate()}, 
-						#input_time{id=startTime, required=true, value=selectedStartTime()}]},
-					#td{body=[#date{id=endDate, required=false, value=selectedEndDate()}, 
-						#input_time{id=endTime, required=false, value=selectedEndTime()}]},
-					#td{body=[#number{id=numberOfSolutions, required=true, step=1, max=100, min=1, value=selectedNumberOfSolutions()}]}
+					#td{body=[#select{name=diveSite, id=diveSite, body=selectable_divesites(), multiple=false, required=true}]},
+					#td{body=[#date{name=startDate, id=startDate, required=true, value=selectedStartDate()}, 
+						#input_time{name=startTime, id=startTime, required=true, value=selectedStartTime()}]},
+					#td{body=[#date{name=endDate, id=endDate, required=false, value=selectedEndDate()}, 
+						#input_time{name=endTime, id=endTime, required=false, value=selectedEndTime()}]},
+					#td{body=[#number{name=numberOfSolutions, id=numberOfSolutions, required=true, step=1, max=100, min=1, value=selectedNumberOfSolutions()}]}
 				]}],
 
 			footer=[
@@ -33,26 +33,60 @@ content() -> [#form{body=[#table{class=["standardTable"],
 				]},
 				#tr{cells=[
 					#td{colspan="4", class=["centered"], body=[
-						#submit{body="Search Times"}
+						#button{id=search, name=search, body="Search Times", 
+							source=[diveSite, startDate, startTime, endDate, endTime, numberOfSolutions],
+							postback=search}
 					]}
 				]}]
 		}]}].
 
 
 event(search) ->
-	[#span{body="Hello!"}].
+	wf:insert_bottom([#span{body="Hello!"}]).
 
 selectable_divesites() ->
 	[#option{label=Site#divesite.name, value=Site#divesite.id, selected=(selectedDiveSite() == Site#divesite.id)} || Site <- divesites:list()].
 
-selectedStartDate() -> [].
+selectedStartDate() -> 
+	case wf:q(startDate) of
+		undefined -> toDateString(calendar:universal_time());
+		E -> E
+	end.
 
-selectedStartTime() -> [].
+selectedStartTime() ->
+	case wf:q(startTime) of
+		undefined -> toTimeString(calendar:universal_time());
+		E -> E
+	end.
 
-selectedEndDate() -> [].
+selectedEndDate() ->
+	case wf:q(endDate) of
+		undefined -> toDateString(calendar:universal_time());
+		E -> E
+	end.
 
-selectedEndTime() -> [].
 
-selectedNumberOfSolutions() -> [].
+selectedEndTime() ->
+	case wf:qs(endTime) of
+		undefined -> toTimeString(calendar:universal_time());
+		E -> E
+	end.
 
-selectedDiveSite() -> [].
+selectedNumberOfSolutions() ->
+	case wf:q(numberOfSolutions) of
+		undefined -> "10";
+		E -> E
+	end.
+
+selectedDiveSite() ->
+	case wf:q(diveSite) of
+		undefined -> 
+			[First, Rest] = divesites:list(),
+			First#divesite.id;
+		E -> E
+	end.
+
+
+toDateString(Date) -> "2014-11-05".
+
+toTimeString(Time) -> "12:01".
