10e8745913ec697e122d9ff33a07df8fe3fb71aa
Archis Gore <archis@concurix.com>
2016-01-25 23:44:59 -0800

    Start script

--- start.bat
+++
@@ -1,2 +0,0 @@
-call mad compile
-call erl -pa ebin deps/ranch/ebin deps/cowboy/ebin deps/cowlib/ebin deps/erlydtl/ebin deps/gproc/ebin deps/n2o/ebin deps/edate/ebin deps/mochiweb/ebin deps/pgsql/ebin deps/mochiweb/ebin deps/mochiweb_xpath/ebin -s divepredictor
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-rebar3 compile
 rebar3 release
 ./_build/default/rel/divepredictor/bin/divepredictor -noshell -noinput
